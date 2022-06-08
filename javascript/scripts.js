f0f889a2da7a244588dfeeb1f894060429214c71
ghostly08 <steven.produccionbcn@gmail.com>
2022-06-08 13:09:22 +0200

    adding closer function to popup

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,6 +1,16 @@
+// ::::::::::: Open Popup :::::::::: //
 
 function displayAbout() {
   const popUp = document.querySelector("#about-popup");
   const openPopup = popUp.style.display = "block"
-  console.log(openPopup);
+}
+
+
+// ::::::::::: Close Popup :::::::: //
+
+function closePopup() {
+  const popUp = document.querySelector("#about-popup");
+  const closePopup = popUp.style.display = "none"
+
+  console.log(closerElement);
 }
